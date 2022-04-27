0f84c6864591511971c7d8d12f1b2a4918bf920e
kimain050401 <kimain0401@dimigo.hs.kr>
2022-04-27 11:04:07 +0900

    Initial commit

---
+++ dimigoin-status/dimigoin_statusApp.swift
@@ -0,0 +1,17 @@
+//
+//  dimigoin_statusApp.swift
+//  dimigoin-status
+//
+//  Created by 김아인 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct dimigoin_statusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
